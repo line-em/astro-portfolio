023d08c1ecaa44ac611092a305e8a447cc2adfca
Line-em <alineemily@hotmail.com>
2023-01-26 21:43:30 +0000

    navbar and hamburguer fully functional

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,51 @@
 import React from "react";
-import { Envelope, FolderNotchOpen, User, Hamburger } from "phosphor-react";
+import { Envelope, FolderNotchOpen, User, Hamburger, X } from "phosphor-react";
 import "../styles/nav.css";
 
 const Nav = () => {
-	return (
+	const [toggleMobileMenu, setToggleMobileMenu] = React.useState(false);
+	const menuItems = (
 		<>
-			<nav>
-				<h2>Aline Emily.</h2>
-				<ul className="mobile-menu">
-					<li>
-						<Hamburger size={24} color="var(--secondary)" weight="duotone" />
-					</li>
-				</ul>
-				<ul className="big-menu">
-					<li>
-						<User weight="duotone" size={24} color="var(--secondary)" />
-						<a href="">About Me</a>
-					</li>
-					<li>
-						<FolderNotchOpen
-							weight="duotone"
-							size={24}
-							color="var(--secondary)"
-						/>
-						<a href="">Projects</a>
-					</li>
-					<li>
-						<Envelope weight="duotone" size={24} color="var(--secondary)" />
-						<a href="">Contact</a>
-					</li>
-					<li>
-						<a href="">Socials</a>
-					</li>
-				</ul>
-			</nav>
+			<li>
+				<User weight="duotone" size={24} color="var(--secondary)" />
+				<a href="">About Me</a>
+			</li>
+			<li>
+				<FolderNotchOpen weight="duotone" size={24} color="var(--secondary)" />
+				<a href="">Projects</a>
+			</li>
+			<li>
+				<Envelope weight="duotone" size={24} color="var(--secondary)" />
+				<a href="">Contact</a>
+			</li>
+			<li>
+				<a href="">Socials</a>
+			</li>
 		</>
+	);
+
+	return (
+		<nav>
+			<h2>Aline Emily.</h2>
+			<button
+				className="mobile-menu"
+				onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
+			>
+				<Hamburger size={40} color="var(--secondary)" weight="duotone" />
+			</button>
+			<div className="overlay" style={{ width: toggleMobileMenu ? "100vw" : "0" }}>
+				<ul className="overlay-content">
+					<button
+						className="mobile-menu"
+						onClick={() => setToggleMobileMenu(!toggleMobileMenu)}
+					>
+						<X size={40} color="var(--secondary)" weight="duotone" />
+					</button>
+					{menuItems}
+				</ul>
+			</div>
+			<ul className="big-menu">{menuItems}</ul>
+		</nav>
 	);
 };
 
