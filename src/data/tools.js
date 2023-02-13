eb12ecc91d766cbbad6319e8be8278df96370cfd
Line-em <alineemily@hotmail.com>
2023-02-13 20:27:20 +0000

    add toolkit

--- src/data/tools.js
+++ src/data/tools.js
@@ -6,89 +6,76 @@ export default [
 		abbr: "HTML - HyperText Markup Language",
 		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
 		altText: "HTML5 icon",
-		text: "HTML5",
-		txtcolor: "#f9f8fc",
-		bgcolor: "orangered"
+		text: "HTML5"
 	},
 	{
 		id: 2,
 		abbr: "CSS - Cascading Style Sheets",
 		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
 		altText: "CSS icon",
-		text: "CSS",
-		bgcolor: "#1570b2",
-		txtcolor: "#f9f8fc"
+		text: "CSS"
 	},
 	{
 		id: 3,
 		abbr: "JavaScript",
 		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
 		altText: "JavaScript icon",
-		text: "JavaScript",
-		txtcolor: "#221126",
-		bgcolor: "#f0ca4f"
+		text: "JavaScript"
 	},
 	{
 		id: 4,
 		abbr: "React",
-		src: "/portfolio/icon/react.svg",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
 		altText: "React icon",
-		text: "React",
-		bgcolor: "#1b9fc4",
-		txtcolor: "#f9f8fc"
+		text: "React"
 	},
 	{
 		id: 5,
 		abbr: "Styled Components",
-		src: "/portfolio/icon/react.svg",
+		src: "https://raw.githubusercontent.com/styled-components/brand/master/styled-components.svg",
 		altText: "Styled Components icon",
-		text: "Styled Components",
-		bgcolor: "#1b9fc4",
-		txtcolor: "#f9f8fc"
+		text: "Styled Components"
 	},
 	{
 		id: 6,
 		abbr: "Astro",
-		src: "/portfolio/icon/react.svg",
+		src: "https://astro.build/assets/press/logomark-light.svg",
 		altText: "Astro icon",
-		text: "Astro",
-		bgcolor: "#1b9fc4",
-		txtcolor: "#f9f8fc"
+		text: "Astro"
 	},
 	{
 		id: 7,
 		abbr: "Yarn",
-		src: "/portfolio/icon/yarn.svg",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg",
 		altText: "Yarn Icon",
-		text: "Yarn & Npm",
-		bgcolor: "#2c8ebb",
-		txtcolor: "#f9f8fc"
+		text: "Yarn & Npm"
 	},
 	{
 		id: 8,
 		abbr: "Git / Github",
 		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
 		altText: "Git Icon",
-		text: "Git & Github",
-		bgcolor: "#f34f29",
-		txtcolor: "#f9f8fc"
+		text: "Git & Github"
 	},
 	{
 		id: 9,
 		abbr: "REST APIs",
-		src: "/portfolio/icon/insomnia.svg",
+		src: "https://www.svgrepo.com/show/353904/insomnia.svg",
 		altText: "Insomnia Icon",
-		text: "REST APIs",
-		bgcolor: "#3f00bd",
-		txtcolor: "#f9f8fc"
+		text: "REST APIs / JSON"
 	},
 	{
 		id: 10,
-		abbr: "Windows",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg",
-		altText: "Windows Icon",
-		text: "Windows",
-		bgcolor: "#00adef",
-		txtcolor: "#f9f8fc"
+		abbr: "Tailwind CSS",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+		altText: "Tailwind CSS",
+		text: "Tailwind CSS"
+	},
+	{
+		id: 10,
+		abbr: "Some Other",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+		altText: "Tailwind CSS",
+		text: "Tailwind CSS"
 	}
 ];
