a789af77839fac99ed9dbacf83a69a50318e6497
Line-em <alineemily@hotmail.com>
2023-02-16 12:35:36 +0000

    add netflix info

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,16 +3,16 @@ export default [
 		id: 0,
 		title: "Netflix Clone",
 		description:
-			"Generate customized quizzes and check how you've fared! It gathers data from an API and validates it. The app has a light and a dark mode, an animated svg background, and various visual feedbacks depending on user state! It also celebrates your victory 🎉",
-		tech: ["React", "Javascript", "Open Trivia API", "CSS Grid", "CSS animation"],
+			"A Netflix Clone with a personal touch. I used Tailwind and Firebase, specially Auth and Databases. I added modals, customizable homepage with various random categories, useRef, loading and error states, toast notifications, customizable alerts with Sweet Alert, extra hover and active effects like CSS transforms, and more.",
+		tech: ["React", "REST API", "Tailwind", "CSS Grid", "Firebase"],
 		image: [
 			`https://webdevamin.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdmf2pac9w%2Fimage%2Fupload%2Fv1669052910%2Fbakiji_construct_portfolio_fe4005b9f4.png&w=2048&q=75`,
 			`${import.meta.env.BASE_URL}/project/quizzical.png`,
 			`${import.meta.env.BASE_URL}/project/quizzical2.png`,
 			`${import.meta.env.BASE_URL}/project/quizzical3.png`
 		],
-		github: "https://github.com/line-em/Quizzical",
-		live: "https://line-em.github.io/Quizzical/"
+		github: "https://github.com/line-em/react-netflix",
+		live: "https://line-em.github.io/react-netflix/"
 	},
 	{
 		id: 1,
@@ -33,14 +33,14 @@ export default [
 		id: 2,
 		title: "Cat Pics E-commerce",
 		description:
-			"This is a mock e-commerce website using various React hooks, such as useContext. It generates various Cat Prints with random titles and prices, with the fun Cat API! I made a CSS Modal, used the reliable React Router Dom and had fun with CSS Grid again.",
+			"This is an e-commerce website using useContext. It generates various Cat Prints with random titles and prices, with the fun Cat API! I made a CSS Modal, used the reliable React Router Dom and had fun with CSS Grid again.",
 		tech: [
 			"React",
 			"React PropTypes",
 			"React Router Dom",
-			"The Cat API",
+			"REST API",
 			"CSS Grid",
-			"Modal"
+			"Modals"
 		],
 		image: [
 			`${import.meta.env.BASE_URL}/project/catpics1.png`,
